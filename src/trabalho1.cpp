af21b675a208f513cbe1f1259ce129e2b02aedd6
Cassiano <honoriocassiano@gmail.com>
2016-09-15 15:59:07 -0300

    Adição dos quadrados dos pontos da função

--- src/trabalho1.cpp
+++ src/trabalho1.cpp
@@ -1,11 +1,11 @@
 /*
-	Name: test01.2.Two.Windows.cpp
-	Author: Rodrigo Luis de Souza da Silva
-	Author: Bruno José Dembogurski
-	Date: 27/01/2011
-	Last Update: 27/01/2016
-	Description: Managing two windows using PixelLab.
-*/
+ Name: test01.2.Two.Windows.cpp
+ Author: Rodrigo Luis de Souza da Silva
+ Author: Bruno Josï¿½ Dembogurski
+ Date: 27/01/2011
+ Last Update: 27/01/2016
+ Description: Managing two windows using PixelLab.
+ */
 
 #include "pixelLab.h"
 #ifdef WIN32
@@ -51,7 +51,7 @@ void idle() {
 
 static void display1(void) {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -63,55 +63,55 @@ static void display1(void) {
 
 	// draw left vertical color box
 	glBegin(GL_QUADS);
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2d(0, 0);
-		glVertex2d(16, 0);
-		glVertex2d(16, 232);
-		glVertex2d(0, 232);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2d(0, 0);
+	glVertex2d(16, 0);
+	glVertex2d(16, 232);
+	glVertex2d(0, 232);
 	glEnd();
 
 	glBegin(GL_QUADS);
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2d(1, 1);
-		glVertex2d(15, 1);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glVertex2d(15, 231);
-		glVertex2d(1, 231);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2d(1, 1);
+	glVertex2d(15, 1);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2d(15, 231);
+	glVertex2d(1, 231);
 	glEnd();
 
 	// draw bottom horizontal color box
-		glBegin(GL_QUADS);
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2d(24, 240);
-		glVertex2d(24, 256);
-		glVertex2d(256, 256);
-		glVertex2d(256, 240);
+	glBegin(GL_QUADS);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2d(24, 240);
+	glVertex2d(24, 256);
+	glVertex2d(256, 256);
+	glVertex2d(256, 240);
 	glEnd();
 
 	glBegin(GL_QUADS);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glVertex2d(25, 241);
-		glVertex2d(25, 255);
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2d(255, 255);
-		glVertex2d(255, 241);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2d(25, 241);
+	glVertex2d(25, 255);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2d(255, 255);
+	glVertex2d(255, 241);
 	glEnd();
 
 	// draw center control box
 	glBegin(GL_QUADS);
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glVertex2d(24, 0);
-		glVertex2d(256, 0);
-		glVertex2d(256, 232);
-		glVertex2d(24, 232);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glVertex2d(24, 0);
+	glVertex2d(256, 0);
+	glVertex2d(256, 232);
+	glVertex2d(24, 232);
 	glEnd();
 
 	glBegin(GL_QUADS);
-		glColor3f(0.94f, 0.94f, 0.94f);
-		glVertex2d(25, 1);
-		glVertex2d(255, 1);
-		glVertex2d(255, 231);
-		glVertex2d(25, 231);
+	glColor3f(0.94f, 0.94f, 0.94f);
+	glVertex2d(25, 1);
+	glVertex2d(255, 1);
+	glVertex2d(255, 231);
+	glVertex2d(25, 231);
 	glEnd();
 
 	// draw dashed lines
@@ -123,16 +123,16 @@ static void display1(void) {
 	// vertical dashed lines
 	for (int i = 1; i < DASHED_AMOUNT; ++i) {
 		glBegin(GL_LINES);
-			glVertex2f(BOX_LEFT + (i * DASHED_SPACING), BOX_TOP);
-			glVertex2f(BOX_LEFT + (i * DASHED_SPACING), BOX_BOTTOM);
+		glVertex2f(BOX_LEFT + (i * DASHED_SPACING), BOX_TOP);
+		glVertex2f(BOX_LEFT + (i * DASHED_SPACING), BOX_BOTTOM);
 		glEnd();
 	}
 
 	// horizontal dashed lines
 	for (int i = 1; i < DASHED_AMOUNT; ++i) {
 		glBegin(GL_LINES);
-			glVertex2f(BOX_LEFT, BOX_TOP + (i * DASHED_SPACING));
-			glVertex2f(BOX_RIGHT, BOX_TOP + (i * DASHED_SPACING));
+		glVertex2f(BOX_LEFT, BOX_TOP + (i * DASHED_SPACING));
+		glVertex2f(BOX_RIGHT, BOX_TOP + (i * DASHED_SPACING));
 		glEnd();
 	}
 
@@ -144,20 +144,38 @@ static void display1(void) {
 	int i = 0;
 
 	glBegin(GL_LINE_STRIP);
-		glColor3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 0.0f);
 
-		for (int x = 0; x < 256; ++x) {
-			while(x > pts[i].x) ++i;
+	for (int x = 0; x < 256; ++x) {
+		while (x > pts[i].x)
+			++i;
 
-			if (!i) dy = pts[i].x? pts[i].y / pts[i].x : 0;
-			else dy = (pts[i].y - pts[i - 1].y) / (pts[i].x - pts[i - 1].x);
+		if (!i)
+			dy = pts[i].x ? pts[i].y / pts[i].x : 0;
+		else
+			dy = (pts[i].y - pts[i - 1].y) / (pts[i].x - pts[i - 1].x);
 
-			fx += dy;
+		fx += dy;
 
-			glVertex2f(BOX_LEFT + (x / 256.0f) * BOX_SIZE,
-					BOX_BOTTOM - (fx / 256.0f) * BOX_SIZE);
-		}
+		glVertex2f(BOX_LEFT + (x / 256.0f) * BOX_SIZE,
+		BOX_BOTTOM - (fx / 256.0f) * BOX_SIZE);
+	}
 	glEnd();
+
+	const float boxSize = 7.0f;
+
+	for (auto& pt : pts) {
+
+		glPushMatrix();
+
+		glTranslatef(BOX_LEFT + (pt.x / 256.0f) * BOX_SIZE,
+		BOX_BOTTOM - (pt.y / 256.0f) * BOX_SIZE, -boxSize / 2);
+
+		glutSolidCube(boxSize);
+
+		glPopMatrix();
+	}
+
 	glutSwapBuffers();
 }
 
@@ -175,45 +193,48 @@ void modifyImage() {
 		for (int x = 0; x < imgOriginal->GetWidth(); x++) {
 			pixelVal = imgOriginal->GetGrayValue(x, y);
 			pixelVal += value;
-			if (pixelVal >= 255) pixelVal = 255;
-			if (pixelVal < 0) pixelVal = 0;
+			if (pixelVal >= 255)
+				pixelVal = 255;
+			if (pixelVal < 0)
+				pixelVal = 0;
 			img->SetGrayValue(x, y, (uByte) pixelVal);
 		}
 	}
 
 	// Update both windows
-	glutPostWindowRedisplay	(window1);
-	glutPostWindowRedisplay	(window2);
+	glutPostWindowRedisplay(window1);
+	glutPostWindowRedisplay(window2);
 }
 
 // Keyboard
 static void key(unsigned char key, int x, int y) {
-	 switch (key) {
-		case 27 :
+	switch (key) {
+	case 27:
 
-		case 'q':
-			exit(0);
+	case 'q':
+		exit(0);
 		break;
-	 }
+	}
 
-	 glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 // Special Keys callback
 void specialKeys(int key, int x, int y) {
-	switch(key) {
-		case GLUT_KEY_UP:
-			brightnessSlider = (brightnessSlider<0) ? 0 : brightnessSlider-1;
-			modifyImage();
+	switch (key) {
+	case GLUT_KEY_UP:
+		brightnessSlider = (brightnessSlider < 0) ? 0 : brightnessSlider - 1;
+		modifyImage();
 		break;
 
-		case GLUT_KEY_DOWN:
-			brightnessSlider = (brightnessSlider>255) ? 255 : brightnessSlider+1;
-			modifyImage();
+	case GLUT_KEY_DOWN:
+		brightnessSlider =
+				(brightnessSlider > 255) ? 255 : brightnessSlider + 1;
+		modifyImage();
 		break;
 
-		case 'q':
-			exit(0);
+	case 'q':
+		exit(0);
 	}
 
 	glutPostRedisplay();
@@ -228,7 +249,7 @@ void mouse(int button, int state, int x, int y) {
 }
 
 // Motion callback - Capture mouse motion when left button is clicked
-void motion(int x, int y ) {
+void motion(int x, int y) {
 	brightnessSlider = y;
 	modifyImage();
 }
@@ -237,22 +258,23 @@ void init() {
 	imgOriginal = new PixelLab();
 	imgOriginal->Read("figs/lenaGray.png");
 
-	Point p1 = {0.0f, 0.0f};
+	Point p1 = { 0.0f, 0.0f };
 	pts.push_back(p1);
 
-	Point p2 = {0.3f * 256, 0.6f * 256};
+	Point p2 = { 0.3f * 256, 0.6f * 256 };
 	pts.push_back(p2);
 
-	Point p3 = {0.6f * 256, 0.3f * 256};
+	Point p3 = { 0.6f * 256, 0.3f * 256 };
 	pts.push_back(p3);
 
-	Point pn = {256.0f, 256.0f};
+	Point pn = { 256.0f, 256.0f };
 	pts.push_back(pn);
 
 	img = new PixelLab();
 	img->Copy(imgOriginal);
 
-	printf("Change brightness clicking on the left window\n or using the 'up' and 'down' keyboard keys.\n");
+	printf(
+			"Change brightness clicking on the left window\n or using the 'up' and 'down' keyboard keys.\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -281,7 +303,7 @@ int main(int argc, char *argv[]) {
 	// Display Window
 	glutSetWindow(window2); // Change current window to 2
 	glutDisplayFunc(display2);
-	glutReshapeWindow(imgOriginal->GetWidth(),imgOriginal->GetHeight());
+	glutReshapeWindow(imgOriginal->GetWidth(), imgOriginal->GetHeight());
 	glutPositionWindow(25 + CWIDTH, 30);
 	glutKeyboardFunc(key);
 
